cb3480c0d323c11830ab4b4130458d65886125d2
Oleh3131 <oshevskyioleh@gmail.com>
2022-05-11 19:11:15 +0300

    new info

---
+++ src/app/users/users.component.ts
@@ -0,0 +1,19 @@
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+
+@Component({
+  selector: 'app-users',
+  templateUrl: './users.component.html',
+  styleUrls: ['./users.component.css']
+})
+export class UsersComponent {
+
+  constructor(private http: HttpClient) {
+
+  }
+
+  getUsers(): Observable<any[]> {
+
+  }
+}
