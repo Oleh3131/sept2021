8284be50cbebb336ff9d52f347a41ae7c8475ba3
Oleh3131 <oshevskyioleh@gmail.com>
2022-06-01 14:42:36 +0300

    new info

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {FormControl, FormGroup} from "@angular/forms";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-register',
@@ -8,7 +8,7 @@ import {FormControl, FormGroup} from "@angular/forms";
 })
 export class RegisterComponent implements OnInit {
 
-  // Потрібна зміна типу(створюються програмно,це один із видів реактивних форм):
+  //1. Потрібна змінна типу(створюється програмно,це один із видів реактивних форм):
 
   form: FormGroup;
 
@@ -21,12 +21,16 @@ export class RegisterComponent implements OnInit {
   ngOnInit(): void {
   }
 
-// треба проініціалізувати форму якимись філдами тобто за що будуть відповідати конкретні поля.
+
+
+
+// 2. треба проініціалізувати форму якимись філдами тобто за що будуть відповідати конкретні поля.
 //   Створимо якийсь медод _createForm (в себе приймати нічого не буде і віддавати нічного не буде):
+
 
   _createForm(): void {
 
-    // Звертаємось до нашої змінної form і вона буде дорівнювати new FormGroup({}).
+    // 3. Звертаємось до нашої змінної form і вона буде дорівнювати new FormGroup({}).
     // Він в себе буде приймати обєкт і грубо говорячи це і буде вся наша форма в ts коді.
     // А в середину ми будемо класти контролери які будуть відповідати за інпути.
     //Ми будемо працювати з API карів і в нас будуть філди username та password.
@@ -36,12 +40,34 @@ export class RegisterComponent implements OnInit {
     // І в конструкторі ініціалізуємо це все (тобто запускаємо наш метод)
 
 
+
+    //6.Можемо також валідувати нашу форму, за це також відповідає FormControl,други параметром може ідти [Validators]
+    //Ми можемо це використовувати і сказати що наша кнопка буде disabled якщо форма буде не валідною.
+
+
     this.form = new FormGroup({
-      username: new FormControl(null),
-      password: new FormControl(null),
-      confirmPassword: new FormControl(null)
+      username: new FormControl(null,[Validators.minLength(3),Validators.maxLength(20)]),
+      password: new FormControl(null,[Validators.minLength(3),Validators.maxLength(20)]),
+      confirmPassword: new FormControl(null,[Validators.minLength(3),Validators.maxLength(20)])
     })
 
   }
+
+  register(): void {
+
+    console.log(this.form)
+
+  };
+
+
+
+  //   <!--
+  //     7.Але нам потрібно ще й шоб password та confirmPassword були валідними тобто співпадали між собою.
+  //   Для цього ми можемо створити свій кастомний валідатор.
+  // -->
+
+
+
+
 
 }
